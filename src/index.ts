8a70253f23d0208a28f2ba5005fc796696377f6d
anibal andrade <anibal.campos@unplug.studio>
2024-09-29 18:50:52 -0600

    feat: Add user courses API with GET and POST methods

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,9 @@ app.post("/api/save-user", async (req, res) => {
 		.upsert(
 			{ google_id, email, display_name, profile_picture_url, last_login },
 			{ onConflict: "google_id" }
-		);
+		)
+		.select("*")
+		.single();
 
 	if (error) {
 		res.status(500).json({ error: error.message });
@@ -27,6 +29,44 @@ app.post("/api/save-user", async (req, res) => {
 		res.status(200).json({ data });
 	}
 });
+
+app
+	.route("/api/courses")
+	.get(async (req, res) => {
+		const { user_id } = req.query;
+
+		if (!user_id) {
+			res.status(400).json({ error: "User ID is required" });
+		}
+
+		const { data, error } = await supabase
+			.from("courses")
+			.select("*")
+			.eq("user_id", user_id);
+
+		if (error) {
+			res.status(500).json({ error: error.message });
+		} else {
+			res.status(200).json({ data });
+		}
+	})
+	.post(async (req, res) => {
+		const { user_id, course_name } = req.body;
+		if (!user_id || !course_name) {
+			res.status(400).json({ error: "Not enough arguments" });
+		} else {
+			const { data, error } = await supabase
+				.from("courses")
+				.insert({ user_id, course_name })
+				.select("*")
+				.single();
+			if (error) {
+				res.status(500).json({ error: error.message });
+			} else {
+				res.status(201).json({ data });
+			}
+		}
+	});
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
