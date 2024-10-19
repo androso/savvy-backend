45108dda98933fd55747a034893fb18ca468ef9d
androso <androso421@gmail.com>
2024-10-19 14:46:39 -0600

    Add credentials option to CORS middleware

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import userRoutes from "./routes/userRoutes";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
