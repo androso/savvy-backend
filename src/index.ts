d90c9bf23168e7749c5c9a8af36ceb776d3eb024
androso <androso421@gmail.com>
2024-10-31 09:26:17 -0600

    adding middleware to handle errors thrown

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import courseRoutes from "./routes/courseRoutes";
 import userRoutes from "./routes/userRoutes";
-import flashcard from "./routes/flashcard"
+import flashcard from "./routes/flashcard";
 import swaggerEndPoint from "./swagger";
 
 const app = express();
@@ -17,10 +17,14 @@ app.use(cookieParser());
 
 app.use("/api/courses", courseRoutes);
 app.use("/api/login", userRoutes);
-app.use('/api/flashcards', flashcard);
+app.use("/api/flashcards", flashcard);
 
+app.use((err: Error, req: Request, res: Response) => {
+	console.error(err.stack);
+	res.status(500).json({ error: err.message ?? "Internal server errror" });
+});
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
-	swaggerEndPoint(app, port)
+	swaggerEndPoint(app, port);
 });
